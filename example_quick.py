74fe6eb9ceb5faa3cc084cb0e07ad7ceec54cffb
gittyeric <git@letsmakeit.com>
2025-02-25 02:01:34 -0800

    better var names

--- example_quick.py
+++ example_quick.py
@@ -7,12 +7,12 @@ from os import path
 def main():
     # Naively derive "reasonable" synthetic derivative images based on the on/off switch images.
     # "Reasonable" here is quick-and-dirty defined by a default one-shot image model under the hood
-    training_img_dir = path.join("examples", "basic", "train", "img")
-    training_set = [path.join(training_img_dir, rel) for rel in os.listdir(training_img_dir)]
+    img_dir = path.join("examples", "basic", "train", "img")
+    img_files = [path.join(img_dir, rel) for rel in os.listdir(img_dir)]
     img_aug = ImageAugmenter()
     # Analyze training set for model weaknesses under different randomizations
-    img_aug.searchRandomizationBoundries(training_set)
+    img_aug.searchRandomizationBoundries(img_files)
     # Generate 50 "reasonable" random permutations of the on/off switch to "generated" directory
-    img_aug.synthesizeMore(training_set, count=50, output_dir="generated")
+    img_aug.synthesizeMore(img_files, count=50, output_dir="generated")
 
 main()
