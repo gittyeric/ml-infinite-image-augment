8545b9f8c735a10caec49a060c4f517a43b9eb61
gittyeric <git@letsmakeit.com>
2025-02-26 01:32:29 -0800

    minor comments

--- example_quick.py
+++ example_quick.py
@@ -2,15 +2,15 @@ from augment import ImageAugmenter
 import os
 from os import path
 
-# Fastest example for getting started generating images with naively and only based on your training data, not the
-# threshold of randomness YOUR model is tolerant to.
+# Fastest example for getting started generating images naively and only based on your training data, not the
+# threshold of randomness your MODEL+dataset is tolerant to.
 def main():
-    # Naively derive "reasonable" synthetic derivative images based on the on/off switch images.
-    # "Reasonable" here is quick-and-dirty defined by a default one-shot image model under the hood
+    # Setup 2 images to generate images from
     img_dir = path.join("examples", "basic", "train", "img")
     img_files = [path.join(img_dir, rel) for rel in os.listdir(img_dir)]
+
     img_aug = ImageAugmenter()
-    # Analyze training set for model weaknesses under different randomizations
+    # Find reasonable random intensity bounds with a dumb default vision model and your dataset of 2 switch images
     img_aug.search_randomization_boundries(img_files)
     # Generate 50 "reasonable" random permutations of the on/off switch to "generated" directory
     img_aug.synthesize_more(img_files, count=50, output_dir="generated")
